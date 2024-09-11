9ff080959c0dc3c90871bbc042b64e0fa87c2591
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-10 23:48:59 -0700

    first commit

--- lib/controller/ui_controller/home.dart
+++ lib/controller/ui_controller/home.dart
@@ -1,4 +1,9 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_sslcommerz/model/SSLCSdkType.dart';
+import 'package:flutter_sslcommerz/model/SSLCTransactionInfoModel.dart';
 import 'package:flutter_sslcommerz/model/SSLCommerzInitialization.dart';
+import 'package:flutter_sslcommerz/model/SSLCurrencyType.dart';
 import 'package:flutter_sslcommerz/sslcommerz.dart';
 import 'package:get/get.dart';
 
@@ -6,14 +11,28 @@ class HomeController extends GetxController {
   Future<void> sslCommerzGeneralCall() async {
     Sslcommerz sslcommerz = Sslcommerz(
       initializer: SSLCommerzInitialization(
-        store_id: "store_id",
-        store_passwd: "store_passwd",
+        ipn_url: "www.ipnurl.com",
+        store_id: "store_id", //account id name ssl
+        store_passwd: "store_passwd", //account id pass ssl
+        multi_card_name: "visa",
         total_amount: 100,
-        currency: "currency",
-        tran_id: "tran_id",
-        product_category: "product_category",
-        sdkType: "sdkType",
+        currency: SSLCurrencyType.BDT,
+        tran_id: "1254789635",
+        product_category: "Food",
+        sdkType: SSLCSdkType.TESTBOX,
       ),
     );
+
+    try {
+      SSLCTransactionInfoModel result = await sslcommerz.payNow();
+
+      if(result.status!.toLowerCase()== "failed"){
+        Fluttertoast.showToast()
+
+      }
+
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
